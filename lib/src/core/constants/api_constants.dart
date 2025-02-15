5116fac31c1c6e43c8bcd3cd1e6b26922f4ce0da
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-02-15 15:18:39 +0100

    Update base URL in API and app constants to correct domain

--- lib/src/core/constants/api_constants.dart
+++ lib/src/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   ApiConstants._(); // Private constructor to prevent instantiation
 
-  static const String baseUrl = 'https://flatcn.com/api/v1';
+  static const String baseUrl = 'https://flutcn.com/api/v1';
 }
