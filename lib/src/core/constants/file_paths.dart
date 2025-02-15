cb5caa54a911ad7aa0e6d09292dad9196be54927
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-02-15 15:01:39 +0100

    Add new theme and palette color files; update file paths in constants

--- lib/src/core/constants/file_paths.dart
+++ lib/src/core/constants/file_paths.dart
@@ -9,14 +9,19 @@ class FilePaths {
   static const String themePath = 'lib/themes'; // Or make this configurable
   // Use path.join
   static final String defaultThemePath =
-      File(path.join('lib/src/core/constants/default_theme.dart')).readAsStringSync();
+      File(path.join('lib/src/core/registries/default_theme.dart'))
+          .readAsStringSync();
   static final String zincThemeFileContent =
-      File(path.join('lib/src/core/constants/zinc_palette_colors.txt')).readAsStringSync();
+      File(path.join('lib/src/core/registries/zinc_palette_colors.txt'))
+          .readAsStringSync();
   static final String newYorkThemeFileContent =
-      File(path.join('lib/src/core/constants/new_york_theme.txt')).readAsStringSync();
+      File(path.join('lib/src/core/registries/new_york_theme.txt'))
+          .readAsStringSync();
 
   static final String slateThemeFileContent =
-      File(path.join('lib/src/core/constants/slate_palette_colors.txt')).readAsStringSync();
+      File(path.join('lib/src/core/registries/slate_palette_colors.txt'))
+          .readAsStringSync();
   static final String grayThemeFileContent =
-      File(path.join('lib/src/core/constants/gray_palette_colors.txt')).readAsStringSync();
+      File(path.join('lib/src/core/registries/gray_palette_colors.txt'))
+          .readAsStringSync();
 }
